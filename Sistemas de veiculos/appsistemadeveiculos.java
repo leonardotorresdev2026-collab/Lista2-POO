d462a4b070be98c14b08f1987ba91c4bc7ec85a6
eotorres1 <leonardo.torres.dev2026@gmail.com>
2026-08-20 12:23:20 -0300

    Update appsistemadeveiculos.java

--- Sistemas de veiculos/appsistemadeveiculos.java
+++ Sistemas de veiculos/appsistemadeveiculos.java
@@ -2,10 +2,12 @@ public class appsistemadeveiculos {
 
     public static void main(String[] args) {
 
-        Carro carro = new Carro("Chevrolet ", Camaro", 2003, 2);
+        Carro carro = new Carro("Chevrolet ", "Camaro", 2003, 2);
 
         Moto moto = new Moto("Kawasaki", "Ninja ZX", 2026,399);
 
+        System.out.println();
+        
         carro.exibirInfo();
 
         System.out.println();
