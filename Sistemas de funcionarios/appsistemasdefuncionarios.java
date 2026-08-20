bd5481c054420ec74befc143c49fb04a985b9da1
eotorres1 <leonardo.torres.dev2026@gmail.com>
2026-08-20 11:49:29 -0300

    Atualizar o appsistemasdefuncionarios.java

--- Sistemas de funcionarios/appsistemasdefuncionarios.java
+++ Sistemas de funcionarios/appsistemasdefuncionarios.java
@@ -2,9 +2,9 @@ public class appsistemasdefuncionarios {
 
     public static void main(String[] args) {
 
-        Gerente gerente = new Gerente("Carlos", 101, 5000, 1000);
+        Gerente gerente = new Gerente("Bruninho Proença", 110, 6000, 1000);
 
-        Vendedor vendedor = new Vendedor("João", 102, 2000, 500);
+        Vendedor vendedor = new Vendedor("berso", 230, 300000, 2000);
 
         System.out.println("Salário do gerente: " + gerente.calcularSalario());
 
