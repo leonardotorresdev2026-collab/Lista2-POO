11c7d4548183b172f074eac8c7f669752e8b6b37
eotorres1 <leonardo.torres.dev2026@gmail.com>
2026-08-20 11:40:46 -0300

    Atualizar o appsistemadeveiculos.java

--- Sistemas de veiculos/appsistemadeveiculos.java
+++ Sistemas de veiculos/appsistemadeveiculos.java
@@ -2,7 +2,7 @@ public class appsistemadeveiculos {
 
     public static void main(String[] args) {
 
-        Carro carro = new Carro("Toyota", "Corolla", 2024, 4);
+        Carro carro = new Carro("Camaro ", "Corolla", 2024, 4);
 
         Moto moto = new Moto("Honda", "CB500", 2023, 500);
 
